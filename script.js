b43b56efcf9654daa25374d61218087411c72ba4
Asmita Mathur <foreveryoung13102006@gmail.com>
2025-12-28 10:10:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const text = "Hey, I made this for you 💗";
+const message = "Just thought you should know — you’re special.";
+
+let i = 0;
+let j = 0;
+
+function typeTitle() {
+  if (i < text.length) {
+    document.getElementById("text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeTitle, 80);
+  }
+}
+
+function typeMessage() {
+  if (j < message.length) {
+    document.getElementById("message").innerHTML += message.charAt(j);
+    j++;
+    setTimeout(typeMessage, 50);
+  }
+}
+
+typeTitle();
+setTimeout(typeMessage, 2000);
